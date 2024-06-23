041641e298341f6195a42a7797a143b8213b1d63
Sally Schaffner <151073463+SallySchaffner@users.noreply.github.com>
2024-06-23 00:09:19 +0000

    Senior Property Tax Program

--- .ccls-cache/@home@runner@Chapter-3-Examples/main.cpp
+++ .ccls-cache/@home@runner@Chapter-3-Examples/main.cpp
@@ -1,4 +1,5 @@
-// Property Tax - Calculates the annual property tax based on the assess value of the property.
+// Senior Property Tax - Calculates the annual property tax based on the assess value of the property and senior
+//                       citizen discounts and calculates the quarterly payment. 
 //
 
 #include <iostream>
@@ -9,18 +10,22 @@ using namespace std;
 int main()
 {
   const double assessmentRate = 0.6;
-  const double taxRate = 0.75;
+  const double taxRate = 2.64;
   const double appliedValue = 100.0;
+  const double seniorDiscount = 5000;
+  const int paymentPeriods = 4;
 
-  double actualValue, assessmentValue, propertyTax;
+  double actualValue, assessmentValue, propertyTax, quarterlyPayment;
 
   cout << "Enter the actual value of the property: $";
   cin >> actualValue;
 
   assessmentValue = actualValue * assessmentRate;
+  assessmentValue -= seniorDiscount;
   propertyTax = assessmentValue / appliedValue * taxRate;
+  quarterlyPayment = propertyTax / paymentPeriods;
 
   cout << fixed << setprecision(2) << showpoint;
-  cout << "Assessed value of the property is $" << setw(7) << assessmentValue << endl;
-  cout << "             Property tax owed is $" << setw(7) << propertyTax << endl;
+  cout << "  Annual tax bill is $" << setw(7) << propertyTax << endl;
+  cout << "Quarterly payment is $" << setw(7) << quarterlyPayment << endl;
 }
