93e20777d77516abd43e19b0bec0a989c11ba47f
Sally Schaffner <151073463+SallySchaffner@users.noreply.github.com>
2024-06-23 00:06:51 +0000

    Property Tax program.

--- .ccls-cache/@home@runner@Chapter-3-Examples/main.cpp
+++ .ccls-cache/@home@runner@Chapter-3-Examples/main.cpp
@@ -1,9 +1,26 @@
-// Chapter 3 Examples
+// Property Tax - Calculates the annual property tax based on the assess value of the property.
+//
+
 #include <iostream>
+#include <iomanip>
+
 using namespace std;
 
-int main() {
-  cout << "Chapter 3 Examples";
+int main()
+{
+  const double assessmentRate = 0.6;
+  const double taxRate = 0.75;
+  const double appliedValue = 100.0;
 
-  return 0;
+  double actualValue, assessmentValue, propertyTax;
+
+  cout << "Enter the actual value of the property: $";
+  cin >> actualValue;
+
+  assessmentValue = actualValue * assessmentRate;
+  propertyTax = assessmentValue / appliedValue * taxRate;
+
+  cout << fixed << setprecision(2) << showpoint;
+  cout << "Assessed value of the property is $" << setw(7) << assessmentValue << endl;
+  cout << "             Property tax owed is $" << setw(7) << propertyTax << endl;
 }
